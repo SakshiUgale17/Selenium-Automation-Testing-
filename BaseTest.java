31a4cd2c321f0a343c6e760ef5e4a856175b6f63
SakshiUgale17 <140419047+SakshiUgale17@users.noreply.github.com>
2025-03-01 19:12:10 +0530

    Add files via upload

---
+++ BaseTest.java
@@ -0,0 +1,26 @@
+package com.Base;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://app-staging.nokodr.com/");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
